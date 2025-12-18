9111a2547dd24e49fec69219893818fafe5d18b7
email <duongnguyen2k3it@gmail.com>
2025-12-18 11:35:07 +0700

    adsfsaasd

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <h1>Thi giữa kỳ</h1>
+        <p>DH52200550_NGUYENTRONGDUONG</p>
+        <img src="./img/avatar.jpg" alt="">
+    </body>
+    </html
